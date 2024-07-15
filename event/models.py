669b400dd134f50580b9e1677bda4821036b2a8e
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-07-15 21:00:47 +0100

    chore: Add default value for date field in Event model
    Changed attribute 'location' to 'venue_address' and updated its type to TextField

--- event/models.py
+++ event/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='event_images/', null=True, blank=True)
-    date = models.DateTimeField()
-    location = models.CharField(max_length=200)
+    date = models.DateTimeField(default=timezone.now)
+    venue_address = models.TextField()
 
     def __str__(self):
         return self.title
