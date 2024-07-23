e3823d3704d5f1d1939a83cdebae712de4349129
Remigius Mgbeme <garethremigius@gmail.com>
2024-07-24 00:36:48 +0100

    Account Serializer

--- account/serializers.py
+++ account/serializers.py
@@ -35,3 +35,50 @@ class AccountSerializer(serializers.ModelSerializer):
 
 class StudentLoginSerializer(serializers.Serializer):
     matric_number = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+# Student Signup Serializer
+class StudentSignupSerializer(serializers.ModelSerializer):
+    class Meta(AccountSerializer):
+        model = Account
+        fields = AccountSerializer.Meta.fields + [
+            "matric_number",
+            "level_year",
+            "current_cgpa"
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+# Lecturer Login Serializer
+class LecturerLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+# Lecturer Signup Serializer
+class LecturerSignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = AccountSerializer.Meta.fields + [
+            "lecturer_title",
+            "academic_role"
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+# Admin Login Serializer
+class AdminLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+# Admin Signup Serializer
+class AdminSignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = AccountSerializer.Meta.fields + [
+            "is_admin_user"
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
