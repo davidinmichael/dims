a9c9319d40e926df75583eb70072541b1030b2f0
Remigius Mgbeme <garethremigius@gmail.com>
2024-07-25 18:05:23 +0100

    send email function

--- account/utils.py
+++ account/utils.py
@@ -10,3 +10,26 @@ def get_auth_token(user):
         'refresh': str(refresh),
         'access': str(refresh.access_token),
     }
+
+
+def send_email(user_email, template, subject):
+    subject = subject
+    from_email = settings.EMAIL_HOST_USER
+    to_email = [user_email]
+
+    email = EmailMultiAlternatives(
+        subject = subject,
+        body = "This is the body of the email",
+        from_email = from_email,
+        to=to_email,
+    )
+
+    email.content_subtype = "text/html"
+    email.attach_alternative(template, "text/html")
+
+    try:
+        email.send(fail_silently=False)
+    except Exception as e:
+        print("Email sending failed", {e})
+        return "Couldn't connect, try again"
+    return None
