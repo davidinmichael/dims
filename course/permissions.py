e77bf91ce6bc3acacce83e7ed21ee478bccaeb31
Remigius Mgbeme <garethremigius@gmail.com>
2024-07-28 09:36:20 +0100

    permission fields corrected

--- course/permissions.py
+++ course/permissions.py
@@ -1,8 +1,8 @@
 from rest_framework import permissions
-
+from account.models import *
 
 class WriteOrRead(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.is_staff or request.user.is_superuser
+        return request.user.is_admin_user or request.user.is_lecturer
