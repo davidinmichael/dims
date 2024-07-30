e625edc7d700a31f209800be13fec2688444d983
davidinmichael <davidinmichael@gmail.com>
2024-07-30 11:01:52 +0100

    Semester field modified

--- account/models.py
+++ account/models.py
@@ -57,7 +57,7 @@ class Account(AbstractUser):
     # Student Fields
     matric_number = models.CharField(max_length=20, null=True, blank=True)
     level_year = models.IntegerField(choices=StudentLevel.choices, null=True, blank=True)
-    current_semester = models.CharField(max_length=10, null=True, blank=True)
+    current_semester = models.IntegerField(choices=SchoolSemester.choices, null=True, blank=True, default=SchoolSemester.FIRST_SEMESTER)
     current_cgpa = models.CharField(max_length=10, null=True, blank=True)
     is_student = models.BooleanField(default=True)
 
--- course/models.py
+++ course/models.py
@@ -15,7 +15,7 @@ class Courses(models.Model):
     course_unit = models.CharField(max_length=20, null=True, blank=True)
     course_code = models.CharField(max_length=20, null=True, blank=True)
     level = models.IntegerField(choices=StudentLevel.choices, null=True, blank=True, default=StudentLevel.LEVEL_ONE)
-    semester = models.CharField(choices=SchoolSemester.choices, max_length=15, null=True, blank=True, default=SchoolSemester.FIRST_SEMESTER)
+    semester = models.IntegerField(choices=SchoolSemester.choices, null=True, blank=True, default=SchoolSemester.FIRST_SEMESTER)
     
     created_by = models.ForeignKey(Account, related_name="created_by_user", on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
