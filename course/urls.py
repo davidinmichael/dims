9b099f774270cde0de8c24f37ca2d6ba39c934d2
davidinmichael <davidinmichael@gmail.com>
2024-08-01 14:12:49 +0100

    UrLs for course added

--- course/urls.py
+++ course/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path("", CourseListCreate.as_view()),
     path("<int:pk>/", CourseDetail.as_view()),
+    path("course_count/", CourseCounts.as_view()),
 ]
