89010887d27e3aba372fc4c1335e5d6d1ee0f80e
davidinmichael <davidinmichael@gmail.com>
2024-08-05 15:29:21 +0100

    Event can now be deleted

--- event/views.py
+++ event/views.py
@@ -46,33 +46,12 @@ class EventDetailView(APIView):
             serializer.save()
             return Response({"message": "Event Updated"}, status.HTTP_200_OK)
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-            
-        
-    
 
-# class EventUpdateView(LoginRequiredMixin, APIView):
-#     """
-#     API view to update an existing event (requires login).
-#     """
-#     permission_classes = [IsAuthenticated, IsCustomAdminUser]
+    def delete(self, request, pk):
+        try:
+            event = Event.objects.get(id=pk)
+        except Event.DoesNotExist:
+            return Response({"message": "No event found"}, status.HTTP_404_NOT_FOUND)
+        event.delete()
+        return Response({"message": "Event Deleted"}, status.HTTP_200_OK)
 
-#     def put(self, request, pk):
-#         event = get_object_or_404(Event, pk=pk, user=request.user)
-#         data = json.loads(request.body)
-#         serializer = EventSerializer(event, data=data, partial=True)
-#         if serializer.is_valid():
-#             event = serializer.save()
-#             return JsonResponse(serializer.data)
-#         return JsonResponse(serializer.errors, status=400)
-
-
-# class EventDeleteView(LoginRequiredMixin, APIView):
-#     """
-#     API view to delete an existing event (requires login).
-#     """
-#     permission_classes = [IsAuthenticated, IsCustomAdminUser]
-
-#     def delete(self, request, pk):
-#         event = get_object_or_404(Event, pk=pk, user=request.user)
-#         event.delete()
-#         return JsonResponse({'message': 'Event deleted successfully'})
