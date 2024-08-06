c5fad402596a78fe5b9b02f2343db87f88d7aa81
BEChukwunwike <blessingechukwunwike@gmail.com>
2024-08-06 22:21:34 +0100

    created an Endpoint for returning all courses for each level and semester

--- course/urls.py
+++ course/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("own_courses/", UserOwnCourses.as_view()),
     path("own_outstanding_courses/", UserOutstandingCourse.as_view()),
     path("add_outstanding_courses/", AddOutstandingCourse.as_view()),
+    path("courses/<str:level>/<str:semester>/", CoursesByLevelAndSemester.as_view()),
 ]
