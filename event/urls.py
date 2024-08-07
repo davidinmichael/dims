b213a7984c6815994a52ce0c65606164015599f4
davidinmichael <davidinmichael@gmail.com>
2024-08-07 11:27:07 +0100

    Removed pycache files

--- event/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("", EventListCreatedView.as_view()),
-    path("<int:pk>/", EventDetailView.as_view()),
-]
--- faculty/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path("", FacultyView.as_view()),
-    path("<int:pk>/", FacultyDetail.as_view())
-]
