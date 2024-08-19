33ef336071f87c54c7e66bb6c841e5b1cd4e8637
Remigius Mgbeme <garethremigius@gmail.com>
2024-08-19 13:47:52 +0100

    updated

--- result/permissions.py
+++ result/permissions.py
@@ -6,6 +6,6 @@ from rest_framework.exceptions import PermissionDenied
 class IsLecturerOfCourse(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if obj.lecturer != request.user:
+        if obj.is_lecturer or obj.is_admin_user != request.user:
             raise PermissionDenied(detail="You are not allowed")
         return True
